5fbd990185cbc7360825d7c7e31a601e1ee89abf
matteo <48250758+matte-a@users.noreply.github.com>
2025-07-13 00:30:16 +0200

    u

--- pihole/patch_pihole.sh
+++ pihole/patch_pihole.sh
@@ -3,11 +3,8 @@ set -e
 
 echo "🔧 Patching Pi-hole to use /data/etc-pihole instead of /etc/pihole"
 
-# Patch main scripts if they hardcode paths
-# Replace /etc/pihole with /data/etc-pihole
-find / -type f -exec sed -i 's|/etc/pihole|/data/etc-pihole|g' {} +
-
-# Replace /etc/dnsmasq.d with /data/etc-dnsmasq.d
-find / -type f -exec sed -i 's|/etc/dnsmasq.d|/data/etc-dnsmasq.d|g' {} +
+# Only patch actual scripts
+find /etc/.pihole/ -type f -exec sed -i 's|/etc/pihole|/data/etc-pihole|g' {} +
+find /etc/.pihole/ -type f -exec sed -i 's|/etc/dnsmasq.d|/data/etc-dnsmasq.d|g' {} +
 
 echo "✅ Patch done"
