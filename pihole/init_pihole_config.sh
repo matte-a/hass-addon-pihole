17841c338dcb3707a473949ff52243907d06472d
matteo <48250758+matte-a@users.noreply.github.com>
2025-07-12 18:59:21 +0200

    u

--- pihole/init_pihole_config.sh
+++ pihole/init_pihole_config.sh
@@ -4,17 +4,20 @@ set -e
 DATA_ETC="/data/etc-pihole"
 DATA_DNS="/data/etc-dnsmasq.d"
 
+# Initialize configs if first run
 if [ ! -f "${DATA_ETC}/setupVars.conf" ]; then
-  echo "🔧 Initializing Pi-hole config"
-  cp -r /etc-pihole/* "${DATA_ETC}/"
+  echo "🔧 Copying default Pi-hole config"
+  mkdir -p "${DATA_ETC}"
+  cp -a /defaults/etc-pihole/* "${DATA_ETC}/"
 fi
 
-if [ ! -L "/etc/pihole" ]; then
-  rm -rf /etc/pihole
-  ln -s "${DATA_ETC}" /etc/pihole
+if [ ! -f "${DATA_DNS}/01-pihole.conf" ]; then
+  echo "🔧 Copying default dnsmasq config"
+  mkdir -p "${DATA_DNS}"
+  cp -a /defaults/etc-dnsmasq.d/* "${DATA_DNS}/"
 fi
 
-if [ ! -L "/etc/dnsmasq.d" ]; then
-  rm -rf /etc/dnsmasq.d
-  ln -s "${DATA_DNS}" /etc/dnsmasq.d
-fi
+# Replace /etc with symlinks to persistent storage
+rm -rf /etc/pihole /etc/dnsmasq.d
+ln -s "${DATA_ETC}" /etc/pihole
+ln -s "${DATA_DNS}" /etc/dnsmasq.d
--- pihole/run.sh
+++ pihole/run.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 set -e
 
-# Copy default config if needed, then symlink
+# Initialize configs and symlinks
 bash /init_pihole_config.sh
 
 echo "🧠 Starting Pi-hole services"
 /usr/local/bin/pihole-FTL &
+
+# Start other services if needed
 exec /s6-init
